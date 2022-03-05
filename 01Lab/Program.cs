7c7f746b9c3e646a0f34874e83092db0d0d8dd51
Beata.Zajac <Beata.Zajac@PK5-18>
2022-03-05 16:05:08 +0100

    Dodaj pliki projektów.

---
+++ 01Lab/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _01Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ulamek a = new Ulamek();
+            Ulamek b = new Ulamek(5, 4);
+        }
+
+   
+
+    }
+}
---
+++ 01Lab/Ulamek.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01Lab
+{
+    public class Ulamek
+    {
+        private int mianownik;
+        private int licznik;
+
+        public Ulamek()
+        {
+            this.mianownik = 1;
+            this.licznik = 2;
+        }
+        public Ulamek(int mianownik, int licznik)
+        {
+            this.mianownik = mianownik;
+            this.licznik = licznik;
+        }
+
+      public Ulamek(Ulamek previousUlamek)
+        {
+            mianownik = previousUlamek.mianownik;
+            licznik = previousUlamek.licznik;
+        }
+
+        public void ToString()
+        {
+
+        }
+
+        public static Ulamek operator +(Ulamek a, Ulamek b) => new Ulamek(a.mianownik * b.licznik + b.mianownik * a.licznik, a.licznik * b.licznik);
+        public static Ulamek operator -(Ulamek a, Ulamek b)
+         => a + (-b);
+    }
+}
